d463b37b5e82e7548f145018915215da75cd72ef
felipe <felipe.gabriel.santos.miranda@gmail.com>
2026-08-31 12:44:19 -0300

    at1

---
+++ exercicio01/src/exercicio01/main.java
@@ -0,0 +1,10 @@
+package exercicio01;
+import javax.swing.JOptionPane;
+public class main {
+	public static void main(String[] args) {
+		
+		for (int i = 212; i <= 250; i = i + 2) {
+			JOptionPane.showMessageDialog(null, i);
+		}
+	}
+}
